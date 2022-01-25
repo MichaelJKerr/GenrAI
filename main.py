d3551a950af538d84f9a645d8d91f862d6df8252
Michael Kerr <40428584@live.napier.ac.uk>
2022-01-25 17:18:53 +0000

    your message

---
+++ main.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+import streamlit as st
+from PIL import Image, ImageOps
+import numpy as np
+
+model = tf.keras.models.load_model('model/GAI_Model.h5')
+class_names = ["Blues", "Classical", "Country" ,"DeathMetal","DoomMetal","DrumNBass","Electronic","Folk","Grime","HeavyMetal","HipHop","Jazz","LoFi","Pop","PsychedelicRock","Punk","Reggae","Rock","Soul"] # fill the rest
+
+st.write("""
+         # GenrAI
+         """
+         )
+file = st.file_uploader("Please upload an image file", type=["jpg"])
+
+def import_and_predict(image_data, model):
+
+    size = (300, 300)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    img_array = tf.keras.utils.img_to_array(image)
+    img_array = tf.expand_dims(img_array, 0)
+
+    prediction = model.predict(img_array)
+    score = tf.nn.softmax(prediction[0])
+    sc1 = 100 * np.max(score)
+    st.write("Image is  ", class_names[np.argmax(score)], " with ", "%.2f" % sc1, "% Accuracy")
+    st.image(image, use_column_width=True)
+
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    prediction = import_and_predict(image, model)
