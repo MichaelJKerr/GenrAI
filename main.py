6fbde52778be170596706d932def126f21c1e328
MichaelJKerr <78160510+MichaelJKerr@users.noreply.github.com>
2022-02-24 17:22:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageOps
 import numpy as np
 
 model = tf.keras.models.load_model('model/GAI_Model2.h5')
-class_names = ["Classical", "Country" ,"Death Metal","Doom Metal","Drum and Bass","Electronic","Grime","Heavy Metal","Hip-Hop","Lo-fi","Pop","Punk","Reggae",,"Soul"]
+class_names = ["Classical", "Country" ,"Death Metal","Doom Metal","Drum and Bass","Electronic","Grime","Heavy Metal","Hip-Hop","Lo-fi","Pop","Punk","Reggae","Soul"]
 
 st.image("Assets/logo.jpg")
 file = st.file_uploader("", type=["jpg"])
